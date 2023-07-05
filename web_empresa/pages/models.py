55a089cd98330fd3053d5f85638fdb6565b00a7c
Victor Rubia Rodriguez <victor.r.r90@gmail.com>
2023-07-05 22:14:18 +0200

    subido

---
+++ web_empresa/pages/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+class Page(models.Model):
+    title = models.CharField(verbose_name="Título", max_length=200)
+    content = RichTextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name= 'Orden', default=0)
+    created = models.DateTimeField(verbose_name="Creado", auto_now_add=True)
+    updated = models.DateField(verbose_name="Actualizado", auto_now=True)
+    class Meta():
+        verbose_name = "página"
+        verbose_name_plural = "páginas"
+        ordering = ['order', 'title']
+
+
+    def __str__(self):
+        return self.title     
+  
